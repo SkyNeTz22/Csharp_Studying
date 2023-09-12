e7168a6a7f810f97fd6d9d9f41860a3b8d3ff3ac
Cosmin-Constantin CIRJAN <SkyNeTz22>
2023-09-12 14:11:22 +0300

    initial commit

---
+++ EntityFramework_First/Models/ShopContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework_First.Models
+{
+    public partial class ShopContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ShopContext()
+        {
+        }
+
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-F8ON76N;User ID=dbuser;Password=dbpass;Database=shop;Trusted_Connection=False;TrustServerCertificate=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("Users");
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EntityFramework_First/Program.cs
@@ -0,0 +1,16 @@
+﻿using EntityFramework_First.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new ShopContext())
+        {
+            var users = context.Users.ToList(); // Retrieve all users
+            foreach (var user in users)
+            {
+                Console.WriteLine($"User: {user.FirstName} {user.LastName}");
+            }
+        }
+    }
+}
